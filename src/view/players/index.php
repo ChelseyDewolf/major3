b2e0001d597193c038f6434c2635c4f9fa4b8b21
Chelsey <dewolfchelsey@gmail.com>
2019-01-22 17:10:22 +0100

    Header gecodeert.

--- src/view/players/index.php
+++ src/view/players/index.php
@@ -1,4 +1,36 @@
 <header>
-<h1><span class="hidden">Internationaal Straattheater Beveren</span></h1>
-<nav></nav>
+  <h1><span class="hidden">Internationaal Straattheater Beveren</span></h1>
+  <div class="logo-nav__container container">
+  <img class="logo" src="././assets/img/logo.png" alt="">
+  <nav class="navigation">
+    <ul class="navigation__list">
+      <li class="navigation__list__item">
+        <a href="">Home</a>
+      </li>
+      <li class="navigation__list__item">
+        <a href="">Programma</a>
+      </li>
+      <li class="navigation__list__item">
+        <a href="">Praktisch</a>
+      </li>
+      <li class="navigation__list__item">
+        <a href="">Contact</a>
+      </li>
+    </ul>
+  </nav>
+  </div>
+  <div class="container slogan-button__container">
+    <p class="slogan">Internationaal <br> Straattheaterfestival <br> Beveren</p>
+    <button class="button">Ontdek</button>
+  </div>
+    <div class="header__design-element container">
+    <img src="././assets/img/design-element.png" alt="">
+    </div>
+    <div class="header__date container">
+    <p>AUG <br> 24 - 25 - 26</p>
+    </div>
+
 </header>
+<section class="countdown">
+  <h2 class="hide">Countdown</h2>
+</section>
